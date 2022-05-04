08141683c528e924d632c90a7024ed4b6c6e15be
Pedro Ivo <cinelli.dev@gmail.com>
2022-05-04 11:39:37 -0300

    feat - added login and register (POST requests)

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URL);
 let db;
 const promise = mongoClient.connect().then(() => {
   db = mongoClient.db(process.env.DB_NAME);
 });
 
-app.post("/cadastrar", (req, res) => {
+//FIX ME - Fazer a verificação se já existe email registrado
+app.post("/cadastrar", async (req, res) => {
   const body = req.body;
-  const { nome, email, senha, confirme } = body;
   const schema = joi.object({
     nome: joi.string().alphanum().min(3).max(30).required(),
     email: joi.string().email().required(),
@@ -30,8 +30,49 @@ app.post("/cadastrar", (req, res) => {
     res.status(400).send(errorsDetails);
     return;
   } else {
-    res.sendStatus(201);
+    try {
+      await db.collection("usuarios").insertOne({
+        nome: body.nome,
+        email: body.email,
+        senha: body.senha,
+        confirme: body.confirme
+      })
+      res.sendStatus(201);
+    } catch {
+      res.sendStatus(500)
+    }
   }
 });
+
+//REMINDER - Provavelmente, futuramente será necessarios fazer o get das entradas/saidas ao logar 
+app.post("/logar", async (req, res) => {
+  const body = req.body
+  
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    senha: joi.any().required(),
+  });
+  const { value, error } = schema.validate(body, { abortEarly: false });
+
+  if (error) {
+    const errorsDetails = error.details.map(object => {return object.message})
+    res.status(400).send(errorsDetails);
+    return;
+  } else {
+    try {
+      const usuario = await db.collection("usuarios").findOne({
+        email: body.email,
+        senha: body.senha,
+      })
+      if(!usuario) {
+        res.sendStatus(404)
+        return
+      }
+      res.sendStatus(200);
+    } catch {
+      res.sendStatus(500)
+    }
+  }
+})
 
 app.listen(5000, console.log("O servidor foi ligado! :)"));
