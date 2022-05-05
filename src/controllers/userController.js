3d8a9085bb412cd6c22fba041db7a9fc81a0079d
Pedro Ivo <cinelli.dev@gmail.com>
2022-05-05 14:20:55 -0300

    feat - added DELETE & PUT entries requests

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,84 @@
-import db from '../db.js'
-import {ObjectId} from 'mongodb'
+import db from "../db.js";
+import { ObjectId } from "mongodb";
 
 export async function postRegistro(req, res) {
-  const {authorization} = req.headers
-  if(!authorization) return res.status(401).send("erro 1");
-  const token = authorization.replace("Bearer", "").trim()
+  const body = { ...req.body, date: Date.now() };
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).send("Token nao encontrado");
+  const token = authorization.replace("Bearer", "").trim();
 
   try {
-    const session = await db.collection("sessoes").findOne({token})
-    if(!session) return res.status(401).send("erro 2");
-    const usuario = await db.collection("usuarios").findOne({_id: new ObjectId(session.userId)})
-    if(!usuario) return res.status(401).send("erro 3");
+    const session = await db.collection("sessoes").findOne({ token });
+    if (!session) return res.status(401).send("Sessao nao encontrada");
+    const usuario = await db
+      .collection("usuarios")
+      .findOne({ _id: new ObjectId(session.userId) });
+    if (!usuario) return res.status(401).send("Usuario nao encontrado");
 
-    await db.collection("usuarios").updateOne({email: usuario.email}, {$push: {registros: req.body}})
+    await db
+      .collection("usuarios")
+      .updateOne({ email: usuario.email }, { $push: { registros: body } });
     res.sendStatus(200);
-    console.log(usuario)
-  } catch(e) {
-    console.log(e, "erro no catch do postRegistro")
-    res.sendStatus(500)
+    console.log(usuario);
+  } catch (e) {
+    console.log(e, "erro no catch do postRegistro");
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteRegistro(req, res) {
+  const body = req.body;
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).send("Token nao encontrado");
+  const token = authorization.replace("Bearer", "").trim();
+
+  try {
+    const session = await db.collection("sessoes").findOne({ token });
+    if (!session) return res.status(401).send("Sessao nao encontrada");
+    const usuario = await db
+      .collection("usuarios")
+      .findOne({ _id: new ObjectId(session.userId) });
+    if (!usuario) return res.status(401).send("Usuario nao encontrado");
+
+    await db
+      .collection("usuarios")
+      .updateOne(
+        { email: usuario.email },
+        { $pull: { registros: { date: body.date } } }
+      );
+    res.sendStatus(200);
+    console.log(usuario);
+  } catch (e) {
+    console.log(e, "erro no catch do postRegistro");
+    res.sendStatus(500);
+  }
+}
+
+export async function putRegistro(req, res) {
+  const body = req.body;
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).send("Token nao encontrado");
+  const token = authorization.replace("Bearer", "").trim();
+
+  try {
+    const session = await db.collection("sessoes").findOne({ token });
+    if (!session) return res.status(401).send("Sessao nao encontrada");
+    const usuario = await db
+      .collection("usuarios")
+      .findOne({ _id: new ObjectId(session.userId) });
+    if (!usuario) return res.status(401).send("Usuario nao encontrado");
+
+    await db
+      .collection("usuarios")
+      .findOneAndUpdate(
+        { email: usuario.email, "registros.date" : body.date },
+        { $set: { "registros.$.evento" : body.evento, "registros.$.valor" : body.valor } } 
+      );
+    res.sendStatus(200);
+    const teste = await db.collection("usuarios").findOne({ _id: new ObjectId(session.userId) });
+    console.log(teste)
+  } catch (e) {
+    console.log(e, "erro no catch do putRegistro");
+    res.sendStatus(500);
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { postCadastrar, postLogar } from "./controllers/authController.js";
-import { postRegistro } from "./controllers/userController.js";
+import { postRegistro, deleteRegistro, putRegistro } from "./controllers/userController.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post("/cadastrar", postCadastrar);
-//REMINDER - Provavelmente, futuramente será necessarios fazer o get das entradas/saidas ao logar
+//REMINDER - Será necessario fazer o get das entradas/saidas apos logar
 app.post("/logar", postLogar);
 
 app.post("/novo-registro", postRegistro);
+app.delete("/deletar-registro", deleteRegistro)
+app.put("/alterar-registro", putRegistro)
 
 app.listen(5000, console.log("O servidor foi ligado! :)"));
