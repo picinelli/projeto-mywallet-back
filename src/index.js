373c2f9b15f1ab0785e7c8650c36958ca7003052
Pedro Ivo <cinelli.dev@gmail.com>
2022-05-09 10:26:57 -0300

    chore: something to push

--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ app.use(authRouter)
 app.use(userRouter)
 
 app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+  console.log("Server running on port " + process.env.PORT + "| :)");
 });
