ceebcc274111457836c0e1bb0fe762851fbd8c9d
Pedro Ivo <cinelli.dev@gmail.com>
2022-05-09 10:23:02 -0300

    chore: adjust code for deploy on heroku

--- src/index.js
+++ src/index.js
@@ -11,4 +11,6 @@ app.use(express.json());
 app.use(authRouter)
 app.use(userRouter)
 
-app.listen(5000, console.log("O servidor foi ligado! :)"));
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
